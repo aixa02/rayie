a091e2a11246746379151d729ffac1320fb6b118
aixa saad <aixa.saad@davinci.edu.ar>
2026-06-30 13:43:45 -0300

    google analitycs

--- js/main.js
+++ js/main.js
@@ -11,4 +11,20 @@ document.addEventListener('alpine:init', () => {
       }, 80)
     }
   }))
+  Alpine.data('carta', () => ({
+    carta: [],
+    idioma: 'es',
+
+    async cargarCarta() {
+      try {
+        const response = await fetch('carta.json')
+        this.carta = await response.json()
+        this.carta = this.carta.categorias
+        
+        } catch (error) {
+          console.error('Error cargando carta:', error)
+        }
+      }
+  }))
 })
+
